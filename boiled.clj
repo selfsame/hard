dcd5cac7bc87e58b67eb209e537ca5806ef54917
selfsame <joseph.parker.1@gmail.com>
2016-04-28 18:13:09 -0400

    poking around

--- boiled.clj
+++ boiled.clj
@@ -1,37 +1,155 @@
  
 (ns hard.boiled
-	(:use hard.core)
 	(:require 
 	 arcadia.core
    [clojure.string :as string])
   (:import [UnityEngine]))
 
- (defn hyphen->camel [charseq]
-	(let [parted (partition-by #(= \- %) charseq)]
-	(apply str (cons (apply str (first parted))
-		(map 
-		(fn [col]
-			(cond (= '(\-) col) nil
-				:else (str (string/upper-case (str (first col)))
-						(apply str (rest col)))))
-		(rest parted))))))
+(def serial-fields (atom {}))
 
-(defn kw->sym [kw]
-	(symbol (hyphen->camel (rest (str kw)))))
+(defprotocol ISerializable
+  (serialize [this])
+  (deserialize [this s]))
+
+(defn type-name [sym] (.Name ^MonoType (resolve sym)))
 
 (defmacro fun-printer [t]
-  (let [v (with-meta (gensym "value")
-                     {:tag t})
-        w (with-meta (gensym "writer")
-                     {:tag 'System.IO.TextWriter})]
+  (let [v (with-meta (gensym "value") {:tag t})
+        w (with-meta (gensym "writer") {:tag 'System.IO.TextWriter})]
     `(fn [~v ~w]
-       (.Write ~w
-               (str "$:) " ~v )))))
+       (.Write ~w (str "#"  ~t " " (serialize ~v))))))
 
-(defmacro install-fun-printer
-  "Register a printer for t with clojure's print-method"
-  [t]
-  `(. ~(with-meta 'print-method
-                  {:tag 'clojure.lang.MultiFn})
+(defmacro install-fun-printer  [t]
+  `(. ~(with-meta 'print-method {:tag 'clojure.lang.MultiFn})
       ~'addMethod ~t (fun-printer ~t)))
 
+(defn constructor-arguments [^MonoType t]
+  (get @serial-fields t))
+
+(defmacro vector-constructor [t]
+  (let [ctor (constructor-arguments (resolve t))
+        params (map #(gensym %) ctor)]
+    (list 'fn [(vec params)]
+       (list* 'list ''new (list 'quote t) params))))
+
+(defmacro install-type-parser [t]
+  `(def ~(symbol (str "parse-" (type-name t)))
+     (vector-constructor ~t)))
+
+(deftype Bird [^{:volatile-mutable true :tag int} age])
+(swap! serial-fields conj {Bird '[age]})
+
+(extend-protocol ISerializable
+  Bird
+  (serialize [this] (str "[" (.age this) "]"))
+  (deserialize [this s] 
+    (Bird. (first (read-string s)))))
+
+(install-type-parser Bird)
+(install-fun-printer Bird)
+(hard.boiled.Bird. 7)
+
+(comment 
+(use 'hard.core)
+(import 'ArcadiaState)
+
+(let [go (create-primitive :cube)
+      c (cmpt+ go ArcadiaState)]
+     (rotation! go (rand-vec 360 360 360))
+    (set! (.state c) (Bird. 2048)))
+
+
+
+
+(macroexpand-1 '(fun-printer Bird))
+(install-fun-printer Bird)
+(deserialize (Bird. 0) (serialize (Bird. 56)))
+
+(read-string (prn-str (Bird. 156)))
+
+
+(deserialize (Bird. 1) "[45]"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(defn longest-constructor-signature
+  "Returns the longest parameter list for all constructors in type t"
+  [^System.MonoType t]
+  (->> (.GetConstructors t)
+       (map #(.GetParameters ^ConstructorInfo %))
+       (sort (fn [a b] (compare (count a)
+                                (count b))))
+       last))
+
+
+
+
+(defmacro reconstructor [targetsym t]
+  (mapv
+    (fn [arg]
+      `(. ~targetsym
+          ~(symbol arg)))
+    (constructor-arguments (resolve t))))
+
+(defmacro type-printer [t]
+  (let [v (with-meta (gensym "value")  {:tag t})
+        w (with-meta (gensym "writer") {:tag 'System.IO.TextWriter})]
+    `(fn [~v ~w]
+       (.Write ~w (str "#" ~(str t) " " (reconstructor ~v ~t))))))
+
+(defmacro install-type-printer [t]
+  `(. ~(with-meta 'print-method {:tag 'clojure.lang.MultiFn})
+      ~'addMethod ~t (type-printer ~t)))
+
+(defmacro serializability [types]
+  `(do
+     ~@(mapcat
+         (fn [t]
+           [`(install-type-printer ~t)
+            `(install-type-parser ~t)])
+         types)))
+
+
+
+
+
+
+
+(comment 
+
+(install-type-parser fun.core.fart)
+
+
+((juxt (comp read-string prn-str) prn) (fun.core.fart. false "bad"))
+
+(read-string "#fun.core.fart [false \"big\"]\r\n")
+(prn-str (fun.core.fart. false "bad"))
+
+((juxt (comp read-string prn-str) prn) (Vector3. 1 2 3))
+
+((vector-constructor fun.core.fart) [false "bad"])
+
+(constructor-arguments fun.core.fart))
